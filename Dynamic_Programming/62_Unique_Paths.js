116f701579fd0d11f645c20b2b4363888aabe4d6
Eric <ericliu93@gmail.com>
2026-01-13 14:42:41 -0500

    add optimized solution to 62 unique paths

--- Dynamic_Programming/62_Unique_Paths.js
+++ Dynamic_Programming/62_Unique_Paths.js
@@ -1,12 +1,9 @@
-// https://leetcode.com/problems/unique-paths
-// TIME COMPLEXITY: O(m * n)
-// SPACE COMPLEXITY: O(m * n)
-
 /**
  * @param {number} m
  * @param {number} n
  * @return {number}
  */
+ 
 var uniquePaths = function(m, n) {
     // this question is asking us to calculate every possible unique path from two points on a grid, the top left to the bottom right. the path can only go down or right.
     // clarifying questions - can m or n be 0 or negative?
@@ -15,6 +12,18 @@ var uniquePaths = function(m, n) {
     // add up the values above and to the left to reach the current cell. can use memo to remember the values!!
     // dynamic programming - solving a complex problem by breaking it down into simpler sub problems and storing the results!!
 
+    // OPTIMIZED SOLUTION
+    // we can further optimize the space complexity because we we actually only need the row above to calculate!!
+    const row = Array(n).fill(1);
+    for (let i = 1; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            row[j] = (row[j - 1] || 0) + row[j] 
+
+        }
+    }
+    return row[n - 1];
+    
+    // FIRST SOLUTION
     // create a grid, defaulting values to 0
     const grid = Array.from({ length: m}, () => Array(n).fill(0));
     // loop through each element in grid and calculate the values, we can do a simple nested loop here because it's going top down, left to right, which is the only way the robot can move.
@@ -27,5 +36,6 @@ var uniquePaths = function(m, n) {
             }
         }
     }
+
     return grid[grid.length - 1][grid[0].length - 1]
 };
