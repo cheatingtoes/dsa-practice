715d75a9eee2c60ece9339827678a0f12fe27a7a
Eric <ericliu93@gmail.com>
2026-02-14 19:23:11 -0500

    add two numbers

---
+++ Linked_List/2_Add_Two_Numbers.js
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/add-two-numbers/
+// TIME COMPLEXITY: O(max(m, n)) where m and n are the lengths of the two linked lists
+// SPACE COMPLEXITY: O(max(m, n)) for the new linked list that is created to store the sum of the two numbers
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    // given a linked list in reverse order, return the sum of the two numbers also reversed
+    // since the list is reversed order we can carry the one's place over without going in reverse
+    // since we start at the ones place we don't have to care about the places
+
+    const dummy = new ListNode(null);
+    let carry = 0;
+    let node = dummy;
+
+    // loop through both elements
+    while (l1 || l2) {
+
+        const l1Val = (l1 && l1.val) || 0;
+        const l2Val = (l2 && l2.val) || 0;
+        const sum = l1Val + l2Val + carry;
+        let nodeVal = sum % 10;
+        if (sum >= 10) carry = 1
+        else carry = 0;
+
+        const newNode = new ListNode(nodeVal);
+        node.next = newNode;
+        node = newNode;
+        if (l1) {
+            l1 = l1.next;
+        }
+        if (l2) {
+            l2 = l2.next;
+        }
+    }
+    if (carry === 1) {
+        const newNode = new ListNode(1);
+        node.next = newNode;
+    }
+    return dummy.next;
+};
